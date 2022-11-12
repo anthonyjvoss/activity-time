38f4cfd0bd05779732d89ea9074be7fce9077b07
Tony Voss <anthonyjvoss@gmail.com>
2022-11-11 22:33:08 -0600

    Add activity section and components

--- src/components/Activities.vue
+++ src/components/Activities.vue
@@ -1,18 +1,19 @@
 <template>
     <div>
         <h2>Independent Activities</h2>
-        <aside>Activities that I can do by myself.</aside>
+        
+        <Info msg='Activities that I can do by myself.'/>
 
         <h2>Guided Activities</h2>
-        <aside>Activites that I can do with someone.</aside>
+        <Info>Activites that I can do with someone.</Info>
 
         <h2>Parent Night Activities</h2>
-        <aside>Activities that I can do on parent night.</aside>
+        <Info>Activities that I can do on parent night.</Info>
     </div>
 </template>
 
 <script setup>
-
+    import Info from './Info.vue'
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/Info.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="info">
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+  msg: String,
+})
+</script>
+
+<style lang="scss" scoped>
+.info {
+    background-color: lightgray;
+    border-left: 6px solid gray;
+    // width: max-content;
+}
+</style>
