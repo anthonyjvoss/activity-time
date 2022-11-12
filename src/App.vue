871733fbf280e9806ba11ab3c3bce73c31cabc25
Tony Voss <whosthevoss@tonys-mbp.lan>
2022-11-11 21:55:47 -0600

    Initial project commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+import Activities from './components/Activities.vue'
+</script>
+
+<template>
+  <!-- <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div> -->
+  <div>
+    <h1 class="main-header">Cohen's Activity Time</h1>
+  </div>
+  <Activities />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.main-header {
+  border-style: solid;
+  border-color: grey;
+  border-radius: 30px;
+  padding: 10px;
+}
+</style>
---
+++ src/components/Activities.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Independent Activities</h2>
+        <aside>Activities that I can do by myself.</aside>
+
+        <h2>Guided Activities</h2>
+        <aside>Activites that I can do with someone.</aside>
+
+        <h2>Parent Night Activities</h2>
+        <aside>Activities that I can do on parent night.</aside>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
